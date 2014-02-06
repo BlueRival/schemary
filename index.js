deb40c9d26e5de43ff28f32197cf5f2e6dcb30de
Anthony Hildoer <anthony@bluerival.com>
2014-02-06 15:17:40 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports.Array = require( './lib/array' );
+module.exports.Integer = require( './lib/date' );
+module.exports.Integer = require( './lib/null' );
+module.exports.Number = require( './lib/number' );
+module.exports.Object = require( './lib/object' );
+module.exports.String = require( './lib/string' );
+module.exports.Object = require( './lib/undefined' );
